7f5a1878ec0375c849ee712d42558bdb4a90f2fb
eslamheshamm <esl2mhesham@gmail.com>
2021-02-10 23:30:42 +0200

    Bio components

--- components/common/Bio/Bio.js
+++ components/common/Bio/Bio.js
@@ -5,10 +5,9 @@ export function Bio({ className }) {
 		<div className={clsx(`flex items-center`, className)}>
 			<img
 				className="flex-shrink-0 mb-0 mr-3 rounded-full w-14 h-14 object-cover"
-				src="https://source.unsplash.com/random"
+				src="https://picsum.photos/200/300"
 				alt="Profile"
 			/>
-
 			<p className="text-base leading-7">
 				Written by <b className="font-semibold">Eslam</b>
 			</p>
